8a31ffacac6e6af539768e00e921390343724558
Velo <84917738+Velocities@users.noreply.github.com>
2022-03-12 14:12:52 -0500

    Fix test comparison operators

--- linuxscript.sh
+++ linuxscript.sh
@@ -5,7 +5,7 @@ echo 'PLEASE NOTE: This script uses the ping command to test the TCP/IP layer an
 DEF_GATE=`ip r | grep 'default via' | awk '{print $3}'`
 #Tests functionality of TCP/IP
 TCP_IP_RES=`ping -c 1 127.0.0.1 | grep 'received' | cut -d, -f2 | awk '{print $1}'`
-if [ $TCP_IP_RES > 0 ]; then
+if [ $TCP_IP_RES -gt 0 ]; then
 TCP_IP_STATE="Functional"
 else
 TCP_IP_STATE="Nonfunctional"
@@ -13,7 +13,7 @@ fi
 echo "TCP/IP: $TCP_IP_STATE"
 #Tests connection to the default gateway
 DEF_GATE_RES=`ping -c 1 $DEF_GATE | grep 'received' | cut -d, -f2 | awk '{print $1}'`
-if [ $DEF_GATE_RES > 0 ]; then
+if [ $DEF_GATE_RES -gt 0 ]; then
 DEF_GATE_STATE="Functional"
 else
 DEF_GATE_STATE="Nonfunctional"
@@ -21,7 +21,7 @@ fi
 echo "Default Gateway Connection: $DEF_GATE_STATE"
 #Tests connection to remote server
 REMOTE_SERV_RES=`ping -c 1 1.1.1.1 | grep 'received' | cut -d, -f2 | awk '{print $1}'`
-if [ $REMOTE_SERV_RES > 0 ]; then
+if [ $REMOTE_SERV_RES -gt 0 ]; then
 REMOTE_SERV_STATE="Functional"
 else
 REMOTE_SERV_STATE="Nonfunctional"
